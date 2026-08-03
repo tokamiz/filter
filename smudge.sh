359e02381b24a70c4e250e6303da2af5a3d13412
Oxana <oxana-spb@web.de>
2026-08-03 17:43:20 +0200

    smudge.sh korrektur

--- smudge.sh
+++ smudge.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed "s|password: [DB_PASSWORD]|password: $(grep password database.yml | cut -d':' -f2)|g"
+DB_PWD=$(grep password database.yml | cut -d : -f2 )
+
+sed -i "s|password: [DB_PASSWORD]|password: ${DB_PWD}|g"
